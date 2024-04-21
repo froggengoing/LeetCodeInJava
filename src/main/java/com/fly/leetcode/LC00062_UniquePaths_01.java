993e5bf630cc6831699fa637a864024d325ab0b7
fanglile <fanglile@tctech.onaliyun.com>
2024-04-21 16:00:42 +0800

    [62]不同路径

--- src/main/java/com/fly/leetcode/LC00062_UniquePaths_01.java
+++ src/main/java/com/fly/leetcode/LC00062_UniquePaths_01.java
@@ -17,14 +17,10 @@ class LC00062_UniquePaths_01 {
 
         int[][] dp = new int[m][n];
         //初始化
-        for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
-        }
-        for (int i = 0; i < n; i++) {
-            dp[0][i] = 1;
-        }
-        dp[0][1] = 1;
-        dp[1][0] = 1;
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+
+        for (int i = 0; i < n; i++) dp[0][i] = 1;
+
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
