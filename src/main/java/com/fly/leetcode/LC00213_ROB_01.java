ea85bacabac81d19f1de40c3e7eb92fefcaa1ae7
fanglile <fanglile@tctech.onaliyun.com>
2024-04-21 14:32:34 +0800

    [213]打家劫舍

---
+++ src/main/java/com/fly/leetcode/LC00213_ROB_01.java
@@ -0,0 +1,26 @@
+package com.fly.leetcode;
+
+/**
+ * [198]打家劫舍	55.0%	Medium	0.0%
+ * https://leetcode.cn/problems/house-robber/description/
+ *
+ * @author fly
+ * @create 2024-04-21-13:55
+ **/
+public class LC00213_ROB_01 {
+
+    public static void main(String[] args) {
+        //System.out.println(new LC00213_ROB_01().rob(new int[]{1}));
+        //System.out.println(new LC00213_ROB_01().rob(new int[]{1, 2, 3, 1}));
+        //System.out.println(new LC00213_ROB_01().rob(new int[]{1, 4, 4, 3, 2, 1}));
+        //System.out.println(new LC00213_ROB_01().rob(new int[]{1, 4, 4, 3, 2, 1, 7}));
+        System.out.println(new LC00213_ROB_01().rob(new int[]{2,1,1,2}));
+    }
+
+    /**
+     * dp[n] = max(dp[n-2]+s[n],dp[n-1])
+     */
+    public int rob(int[] nums) {
+        return 0;
+    }
+}
