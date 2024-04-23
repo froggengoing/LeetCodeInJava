94d56daf4f3c35da3209d359c19716d9dab724d3
fanglile <fanglile@tctech.onaliyun.com>
2024-04-23 23:49:00 +0800

    [494]目标和 动态规划-背包问题

---
+++ src/main/java/com/fly/leetcode/LC00474_FindMaxForm_01.java
@@ -0,0 +1,29 @@
+package com.fly.leetcode;
+
+/**
+ * [474]一和零	66.0%	Medium	0.0%
+ * https://leetcode.cn/problems/ones-and-zeroes/description/
+ *
+ * 给你一个二进制字符串数组 strs 和两个整数 m 和 n 。
+ *
+ * 请你找出并返回 strs 的最大子集的长度，该子集中 最多 有 m 个 0 和 n 个 1 。
+ *
+ * 如果 x 的所有元素也是 y 的元素，集合 x 是集合 y 的 子集 。
+ *
+ * 示例 1：
+ *
+ * 输入：strs = ["10", "0001", "111001", "1", "0"], m = 5, n = 3
+ * 输出：4
+ * 解释：最多有 5 个 0 和 3 个 1 的最大子集是 {"10","0001","1","0"} ，因此答案是 4 。
+ * 其他满足题意但较小的子集包括 {"0001","1"} 和 {"10","1","0"} 。{"111001"} 不满足题意，因为它含 4 个 1 ，大于 n 的值 3 。
+ * @author fly
+ * @create 2024-04-23-20:46
+ **/
+public class LC00474_FindMaxForm_01 {
+
+
+    public int findMaxForm(String[] strs, int m, int n) {
+
+        return 0;
+    }
+}
---
+++ src/main/java/com/fly/leetcode/LC00494_FindTargetSumWays_01.java
@@ -0,0 +1,73 @@
+package com.fly.leetcode;
+
+/**
+ * [494]目标和	48.3%	Medium	0.0%
+ * https://leetcode.com/problems/target-sum
+ * <p>
+ * Runtime 9 ms Beats 67.54% of users
+ * with Java Memory 43.09 MB Beats 49.24% of users with Java
+ *
+ * @author fly
+ * @create 2024-04-23-21:18
+ **/
+public class LC00494_FindTargetSumWays_01 {
+    public static void main(String[] args) {
+        System.out.println(new LC00494_FindTargetSumWays_01().findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3));
+        System.out.println(new LC00494_FindTargetSumWays_01().findTargetSumWays(new int[]{0, 1, 1, 1, 1, 1}, 3));
+        System.out.println(new LC00494_FindTargetSumWays_01().findTargetSumWays(new int[]{1, 5, 1, 1, 1}, 5));
+        System.out.println(new LC00494_FindTargetSumWays_01().findTargetSumWays(new int[]{1000}, -1000));
+        System.out.println(new LC00494_FindTargetSumWays_01().findTargetSumWays(new int[]{1000}, 1000));
+        System.out.println(new LC00494_FindTargetSumWays_01().findTargetSumWays(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 1}, 1));
+    }
+
+    /**
+     * 背包问题
+     * sum(p) - sum(n) = target
+     * sum(p) - sum(n) + sum(p) + sum(n) = target + sum(p) + sum(n)
+     * 2 * sum(p) = target + sum(n)
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        // bug: 负数要纠正为正数，因为是等价的
+        target = Math.abs(target);
+        for (int i = 0; i < nums.length; i++) sum += nums[i];
+        if ((target + sum) % 2 != 0) {
+            return 0;
+        }
+        int realTarget = (target + sum) / 2;
+
+        int[][] dp = new int[nums.length][realTarget + 1];
+
+        // bug 第0行的初始化问题
+        if (nums[0] == 0) {
+            dp[0][0] = 2;
+        } else {
+            dp[0][0] = 1;
+            dp[0][nums[0]] = 1;
+        }
+        // bug 第0列初始化问题
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                dp[i][0] = 2 * dp[i - 1][0];
+            } else {
+                dp[i][0] = dp[i - 1][0];
+            }
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 1; j <= realTarget; j++) {
+                if (j >= nums[i]) {
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[nums.length - 1][realTarget];
+    }
+}
