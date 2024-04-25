55d94e4e805af0f3091e2d4fd9f4ff5f186b2474
fanglile <fanglile@tctech.onaliyun.com>
2024-04-25 13:38:32 +0800

    647. 回文子串 动态规划

--- src/main/java/com/fly/leetcode/LC00647_0034_CountSubstrings_02.java
+++ src/main/java/com/fly/leetcode/LC00647_0034_CountSubstrings_02.java
@@ -3,31 +3,30 @@ package com.fly.leetcode;
 /**
  * 647. 回文子串
  * https://leetcode.cn/problems/palindromic-substrings/description/
+ * <p>
+ * Runtime 32 ms Beats 23.18% of users
+ * with Java Memory 55.16 MB Beats 5.14% of users with Java
  *
- * 22 ms Beats 25.99% of users
- * with Java Memory 45.34 MB Beats 16.29% of users with Java
  * @author fly
  * @create 2024-04-25-12:24
  **/
-public class LC00647_0034_CountSubstrings_01 {
+public class LC00647_0034_CountSubstrings_02 {
 
     public static void main(String[] args) {
-        //System.out.println(new LC00647_0034_CountSubstrings_01().countSubstrings("aba"));
-        System.out.println(new LC00647_0034_CountSubstrings_01().countSubstrings("abba"));
-        System.out.println(new LC00647_0034_CountSubstrings_01().countSubstrings("fdsklf"));
+        System.out.println(new LC00647_0034_CountSubstrings_02().countSubstrings("aba"));
+        System.out.println(new LC00647_0034_CountSubstrings_02().countSubstrings("abba"));
+        System.out.println(new LC00647_0034_CountSubstrings_02().countSubstrings("fdsklf"));
     }
 
     public int countSubstrings(String s) {
 
-        int[][] dp = new int[s.length() + 1][s.length() + 1];
         // dp[i][j]是否是回文串
         boolean[][] dp2 = new boolean[s.length() + 1][s.length() + 1];
-        int max = 0;
+        int count = 0;
         for (int i = s.length(); i >= 1; i--) {
 
             for (int j = i; j <= s.length(); j++) {
                 if (i == j) {
-                    dp[i][j] = 1;
                     dp2[i][j] = true;
                 } else {
                     boolean isEqual = s.charAt(i - 1) == s.charAt(j - 1);
@@ -36,15 +35,11 @@ public class LC00647_0034_CountSubstrings_01 {
                     } else if (isEqual) {
                         dp2[i][j] = dp2[i + 1][j - 1];
                     }
-                    // bug：这里不能只判断 char(i)==char(j)而要考虑[i..j]是否是回文
-                    int tmp = dp2[i][j] ? 1 : 0;
-                    dp[i][j] = dp[i + 1][j] + dp[i][j - 1] - dp[i + 1][j - 1] + tmp;
                 }
-
-                max = Math.max(max, dp[i][j]);
+                if (dp2[i][j]) count++;
             }
         }
-        return max;
+        return count;
     }
 
 
